9d342f09a27f88c73ea32dd0b1ed4b65bb6a56c4
steverainwalker <stefan.ghelec@gmail.com>
2017-05-25 09:13:57 +0300

    First commit

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $FirstREST.justREST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
